7abbb45e260b5e96a98f28dbe1ef5c89db8b5f36
Lucas Silveira <lucasdwbfff@gmail.com>
2024-11-24 21:17:28 -0300

    Create payment_not_found.js

---
+++ payment_not_found.js
@@ -0,0 +1,10 @@
+const page = document;
+const head = page.querySelector("head");
+const body = page.querySelector("body");
+
+/* titulo da pagina */
+const titleElement = page.createElement("title");
+const titleContent = `Payment not found`;
+const titleNode = page.createTextNode(titleContent);
+head.appendChield(titleElement);
+
