c8fe61b0e0c6653bf375e5b2a568d585db4a44cc
Lucas Silveira <lucasdwbfff@gmail.com>
2024-12-01 20:46:15 -0300

    Update checkout_page.js

--- checkout_page.js
+++ checkout_page.js
@@ -1,7 +1,6 @@
 let gen = true;
 
-// const api = "http://127.0.0.1:8080"
-const api = "https://0477-179-108-204-191.ngrok-free.app";
+const api = "http://127.0.0.1:8080"
 const page = document;
 const head = page.querySelector('head');
 const body = page.querySelector('body');
@@ -19,97 +18,85 @@ faviconElement.rel = 'shortcut icon';
 faviconElement.href = 'https://BFlex-financial.github.io/checkout/favicon.png'
 head.appendChild(faviconElement);
 
-/* body */
+/* div */
 body.style = "width: 100vw; min-height: 100vh; display: flex; align-items: center; justify-content: center;";
-
-
-// info
-const info = page.createElement('section.info')
-// const center = page.createElement('section');
-// center.className = 'box';
+const center = page.createElement('div');
+center.className = 'box';
   
-// /* Product info */
-// const product = page.createElement('div');
-// product.className = 'product';
+/* Product info */
+const product = page.createElement('div');
+product.className = 'product';
     
-//   /* Product image */
-//   const thumbnail = page.createElement('img');
-//   thumbnail.className = 'thumbnail';
-//   thumbnail.src = checkout.thumbnail;
-//   product.appendChild(thumbnail);
+  /* Product image */
+  const thumbnail = page.createElement('img');
+  thumbnail.className = 'thumbnail';
+  thumbnail.src = checkout.thumbnail;
+  product.appendChild(thumbnail);
   
-//   /* Info display */
-//   const info = page.createElement('div');
-//     const align = page.createElement('div');
+  /* Info display */
+  const info = page.createElement('div');
+    const align = page.createElement('div');
 
-//       const productName = page.createElement('p');
-//       const nameContent = checkout.name || "Uknown";
-//       const name = page.createTextNode(nameContent);
-//       productName.appendChild(name);
-//       productName.className = 'name'
-//       align.appendChild(productName)
+      const productName = page.createElement('p');
+      const nameContent = checkout.name || "Uknown";
+      const name = page.createTextNode(nameContent);
+      productName.appendChild(name);
+      productName.className = 'name'
+      align.appendChild(productName)
 
-//       const productDescription = page.createElement('p');
-//       const descriptionContent = checkout.description || "Uknown";
-//       const description = page.createTextNode(descriptionContent);
-//       productDescription.appendChild(description);
-//       productDescription.className = 'description'
-//       align.appendChild(productDescription)
+      const productDescription = page.createElement('p');
+      const descriptionContent = checkout.description || "Uknown";
+      const description = page.createTextNode(descriptionContent);
+      productDescription.appendChild(description);
+      productDescription.className = 'description'
+      align.appendChild(productDescription)
 
-//     info.appendChild(align);
-//   product.appendChild(info);
+    info.appendChild(align);
+  product.appendChild(info);
 
-// center.appendChild(product);
+center.appendChild(product);
 
-// /* Payment form */
-// const row = page.createElement('div');
-// row.className = 'row';
+/* Payment form */
+const row = page.createElement('div');
+row.className = 'row';
 
-//   const pixButton = page.createElement('button');
-//   pixButton.className = 'payment-form';
-//   pixButton.innerHTML = '<ion-icon name="grid" style="transform: rotateZ(45deg);"></ion-icon> Pix'
-//   row.appendChild(pixButton)
-//   pixButton.onclick = () => renderDynamic('Pix');
+  const pixButton = page.createElement('button');
+  pixButton.className = 'payment-form';
+  pixButton.innerHTML = '<ion-icon name="grid" style="transform: rotateZ(45deg);"></ion-icon> Pix'
+  row.appendChild(pixButton)
+  pixButton.onclick = () => renderDynamic('Pix');
 
-//   const cardButton = page.createElement('button');
-//   cardButton.className = 'payment-form';
-//   cardButton.innerHTML = '<ion-icon name="card"></ion-icon> Card'
-//   row.appendChild(cardButton)
-//   cardButton.onclick = () => renderDynamic('Card');
+  const cardButton = page.createElement('button');
+  cardButton.className = 'payment-form';
+  cardButton.innerHTML = '<ion-icon name="card"></ion-icon> Card'
+  row.appendChild(cardButton)
+  cardButton.onclick = () => renderDynamic('Card');
 
-//   const googleButton = page.createElement('button');
-//   googleButton.className = 'payment-form';
-//   googleButton.innerHTML = '<ion-icon name="logo-google"></ion-icon> GPay'
-//   row.appendChild(googleButton)
-//   googleButton.onclick = () => renderDynamic('GPay');
+  const googleButton = page.createElement('button');
+  googleButton.className = 'payment-form';
+  googleButton.innerHTML = '<ion-icon name="logo-google"></ion-icon> GPay'
+  row.appendChild(googleButton)
+  googleButton.onclick = () => renderDynamic('GPay');
 
-// center.appendChild(row);
+center.appendChild(row);
 
-// /* Dynamic box */
-// const dynamicContent = page.createElement('div');
-// dynamicContent.className = 'dynamic-content'
-// center.appendChild(dynamicContent);
+/* Dynamic box */
+const dynamicContent = page.createElement('div');
+dynamicContent.className = 'dynamic-content'
+center.appendChild(dynamicContent);
 
-// /* Buy button */ 
+/* Buy button */ 
+const buyButton = page.createElement('button');
+buyButton.innerHTML = '<ion-icon name="cart"></ion-icon> Comprar agora'
+buyButton.className = 'buy-button';
+center.appendChild(buyButton);
 
-// // const pix = {
-// //   literal: String,
-// //   base64: String,
-// //   id: String,
-// //   amount: Number
-// // }
+body.appendChild(center);
 
-// const buyButton = page.createElement('button');
-// buyButton.innerHTML = '<ion-icon name="cart"></ion-icon> Comprar agora'
-// buyButton.className = 'buy-button';
-// center.appendChild(buyButton);
-
-// body.appendChild(center);
-
-// renderDynamic('Pix');
-// if( pix.literal ) {
-//   generate();
-// }
+renderDynamic('Pix');
+if( pix.literal ) {
+  generate();
+}
 
 /* functions */
 
